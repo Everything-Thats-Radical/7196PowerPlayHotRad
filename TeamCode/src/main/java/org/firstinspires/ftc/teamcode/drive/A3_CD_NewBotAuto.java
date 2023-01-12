6b1ec7487c616a6efc09ac8b48e647d3d8c24fdd
Everything-Thats-Radical <96023754+Everything-Thats-Radical@users.noreply.github.com>
2023-01-11 21:54:54 -0600

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/A3_CD_NewBotAuto.java
@@ -0,0 +1,263 @@
+
+
+package org.firstinspires.ftc.teamcode.drive;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
+import android.view.View;
+
+import androidx.annotation.RequiresApi;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.SwitchableLight;
+
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+/* Our 3rd in command, Troy Tomson, has made an amazing piece of art that is this code.
+Although janky, we accept it as beautiful, brave, and stunning. If you don't accept it, you are
+literally codiphobic and want all the poor starving children in Africa to die.
+*/
+
+@Autonomous(name = "A3_CD_ParkRad")
+public class A3_CD_NewBotAuto extends LinearOpMode {
+
+    // create motor and servo objects
+    private Servo clampyBoi = null;
+    private DcMotor STRAIGHTUPPPP = null;
+
+    //final double ticks_per_inch = (1120 / (2.952 * 2 * Math.PI));
+
+    ColorSensor colorSensor;
+    DistanceSensor distanceSensor;
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    @Override
+    public void runOpMode() {
+
+        clampyBoi = hardwareMap.get(Servo.class, "clampyBoi");
+        STRAIGHTUPPPP = hardwareMap.get(DcMotor.class, "STRAIGHTUPPPP");
+
+        clampyBoi.setDirection(Servo.Direction.FORWARD);
+        STRAIGHTUPPPP.setDirection(DcMotor.Direction.REVERSE);
+
+        STRAIGHTUPPPP.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        // get a reference to the color sensor.
+        colorSensor = hardwareMap.get(ColorSensor.class, "sensor_color_distance");
+
+        // get a reference to the distance sensor that shares the same name.
+        distanceSensor = hardwareMap.get(DistanceSensor.class, "sensor_color_distance");
+        // hsvValues is an array that will hold the hue, saturation, and value information.
+
+        float hsvValues[] = {0F, 0F, 0F};
+        // values is a reference to the hsvValues array.
+
+        final float values[] = hsvValues;
+        // sometimes it helps to multiply the raw RGB values with a scale factor
+        // to amplify/attentuate the measured values.
+
+        final double SCALE_FACTOR = 255;
+
+        // get a reference to the RelativeLayout so we can change the background
+        // color of the Robot Controller app to match the hue detected by the RGB sensor.
+        int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
+        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+
+        if (colorSensor instanceof SwitchableLight) {
+            ((SwitchableLight) colorSensor).enableLight(true);
+        }
+
+
+        Pose2d startPose = new Pose2d(0, 0, 0);
+        Pose2d scanPose = new Pose2d(0, -25, 0);
+        drive.setPoseEstimate(startPose);
+
+        // send the info back to driver station using telemetry function.
+
+        // change the background color to match the color detected by the RGB sensor.
+        // pass a reference to the hue, saturation, and value array as an argument
+        // to the HSVToColor method.
+
+        // TRAJECTORY SEQUENCES BUILT
+        //----------------------------------
+
+        TrajectorySequence initialDriveForScan = drive.trajectorySequenceBuilder(new Pose2d())
+                .forward(26,
+                        SampleMecanumDrive.getVelocityConstraint(10, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
+                .build();
+
+
+
+        TrajectorySequence driveToJunction = drive.trajectorySequenceBuilder(initialDriveForScan.end())
+                .forward(22,
+                        SampleMecanumDrive.getVelocityConstraint(10, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
+                .back(13,
+                        SampleMecanumDrive.getVelocityConstraint(10, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
+                .turn(-3.14/2)
+                .forward(26,
+                        SampleMecanumDrive.getVelocityConstraint(10, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
+                .turn(3.14/4)
+                .build();
+
+        TrajectorySequence lockToJunction = drive.trajectorySequenceBuilder(driveToJunction.end())
+                .forward(8,
+                        SampleMecanumDrive.getVelocityConstraint(10, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
+                .build();
+
+        TrajectorySequence retreatFromScore = drive.trajectorySequenceBuilder(lockToJunction.end())
+                .back(8,
+                        SampleMecanumDrive.getVelocityConstraint(10, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
+                .build();
+        TrajectorySequence realign = drive.trajectorySequenceBuilder(retreatFromScore.end())
+                .turn(3.14/2 + 3.14/4)
+                .build();
+
+        TrajectorySequence redZone = drive.trajectorySequenceBuilder(realign.end())
+                .forward(24,
+                        SampleMecanumDrive.getVelocityConstraint(10, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
+                .build();
+        TrajectorySequence blueZone = drive.trajectorySequenceBuilder(realign.end())
+                .forward(2,
+                        SampleMecanumDrive.getVelocityConstraint(10, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
+                .build();
+        
+        TrajectorySequence greenZone = drive.trajectorySequenceBuilder(realign.end())
+                .forward(12,
+                        SampleMecanumDrive.getVelocityConstraint(10, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
+                .build();
+
+
+        waitForStart();
+
+        // START OF AUTO MOVEMENT ----------------------------------------------
+        if (!isStopRequested()) {
+        }
+
+        clawControl("clamp");
+        moveLift("up", 6, .7);
+        drive.followTrajectorySequence(initialDriveForScan); // forward 38
+        String coneColor = "green";
+
+        int totalReds = colorSensor.red();
+        int totalGreens = colorSensor.green();
+        int totalBlues = colorSensor.blue();
+
+        if (totalReds > totalGreens && totalReds > totalBlues) {
+            coneColor = "red";
+        } else if (totalGreens > totalBlues && totalGreens > totalReds) {
+            coneColor = "green";
+        } else if (totalBlues > totalGreens && totalBlues > totalReds) {
+            coneColor = "blue";
+        }
+
+        /* Use telemetry to display feedback on the driver station. We show the red, green, and blue
+         * normalized values from the sensor (in the range of 0 to 1), as well as the equivalent
+         * HSV (hue, saturation and value) values. See http://web.archive.org/web/20190311170843/https://infohost.nmt.edu/tcc/help/pubs/colortheory/web/hsv.html
+         * for an explanation of HSV color. */
+
+        // Troy: The scoreCone trajectory sequence does not score the cone.
+        // Isaiah: Why?
+        // Troy: *Walks away*
+
+        drive.followTrajectorySequence(driveToJunction);// back 3, turn right, forward 38, turn left
+        moveLift("up", 29, .7);
+        drive.followTrajectorySequence(lockToJunction); // forward 5, forward 12
+        clawControl("release");
+        drive.followTrajectorySequence(retreatFromScore);
+        moveLift("down", 29, .7);
+        drive.followTrajectorySequence(realign);
+
+        if (coneColor.equals("red")) {
+            telemetry.addData("Color: ", coneColor);
+            telemetry.update();
+            drive.followTrajectorySequence(redZone);
+
+        } else if (coneColor.equals("green")) {
+            telemetry.addData("Color: ", coneColor);
+            telemetry.update();
+            drive.followTrajectorySequence(greenZone);
+
+        } else if (coneColor.equals("blue")) {
+            telemetry.addData("Color: ", coneColor);
+            telemetry.update();
+            drive.followTrajectorySequence(blueZone);
+
+        } else {
+            String noColor = "Color not detected.";
+            telemetry.addData("Color: ", noColor);
+            telemetry.update();
+        }
+
+        // END OF AUTO MOVEMENT -----------------------------------
+
+    }
+
+
+    public void moveLift(String direction, double height, double power) {
+        double revsToInch = (2 * Math.PI);
+        double ticks_per_inch = (1120 / revsToInch); //TICKS PER INCH MAY BE INCORRECT
+        // THE LIFT WENT HIGHER THAN EXPECTED LAST TIME THIS WAS RUN AND BROKE THE LIFT
+        // FIND ACTUAL TICKS PER INCH BEFORE RUNNING
+        double ticksNeeded = height * ticks_per_inch;
+        double initialPosition = STRAIGHTUPPPP.getCurrentPosition();
+        double currentPosition = STRAIGHTUPPPP.getCurrentPosition();
+        int directionSign = 1;
+        double ticksMoved;
+
+        if (direction.equals("up")) {
+            directionSign = 1;
+        } else if (direction.equals("down")) {
+            directionSign = -1;
+        } else {
+            directionSign = -1;
+        }
+
+        ticksMoved = Math.abs(initialPosition - currentPosition);
+        while (ticksNeeded > ticksMoved) {
+            currentPosition = STRAIGHTUPPPP.getCurrentPosition();
+            ticksMoved = Math.abs(initialPosition - currentPosition);
+
+            telemetry.addData("ticksNeeded ", ticksNeeded);
+            telemetry.addData("ticksMoved ", ticksMoved);
+            telemetry.addData("ticksNeeded - ticksMoved ", ticksNeeded - ticksMoved);
+            telemetry.update();
+
+            STRAIGHTUPPPP.setPower(power * directionSign);
+        }
+        STRAIGHTUPPPP.setPower(0);
+        clawControl("clamp");
+    }
+
+    public void clawControl(String state) {
+        double location = 0;
+
+        if (state.equals("clamp")) {
+            location = 0;
+        } else if (state.equals("release")) {
+            location = 1;
+        }
+        clampyBoi.setPosition(location);
+    }
+}
+
+
